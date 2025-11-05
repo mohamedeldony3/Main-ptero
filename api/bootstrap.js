528b05768b2b351d1e03ccb079f975dd2a2300d6
mohamedeldony3 <60990149+mohamedeldony3@users.noreply.github.com>
2025-11-05 19:21:52 +0200

    Update bootstrap.js

--- api/bootstrap.js
+++ api/bootstrap.js
@@ -1,10 +1,10 @@
-// api/bootstrap.js
 export default function handler(req, res) {
   const script = `#!/usr/bin/env bash
 set -euo pipefail
-echo "Running remote secure task..."
-curl -fsS https://ptero.melsony.site/api/run | bash -s --
+echo "Requesting secure remote execution..."
+TOKEN="63537762"
+curl -fsS -H "Authorization: Bearer $TOKEN" https://ptero.melsony.site/api/run | bash -s --
 `;
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
   res.status(200).send(script);
 }
